27bea15c76a12c624d6957b7139ebf7eb82f181a
Marina Alane <marina.alane@outlook.com>
2021-07-01 17:55:26 -0300

    muda linha localStorage

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -5,7 +5,7 @@ import Header from '../Components/Header';
 import '../Style/Perfil.css';
 
 function Perfil() {
-  const emailLocalStorage = JSON.parse(localStorage.getItem('user'));
+  // const emailLocalStorage = JSON.parse(localStorage.getItem('user')).email;
   const history = useHistory();
 
   function resetProfile() {
@@ -20,7 +20,7 @@ function Perfil() {
         data-testid="profile-email"
         className="email"
       >
-        {emailLocalStorage && emailLocalStorage.email}
+        {localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')).email}
       </div>
       <div className="buttons-container">
         <button
