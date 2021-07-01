ee9ea0848e1681bab9a45fcfa369d750eed2422b
Marina Alane Santana de Morais <70583230+MarinaAlane@users.noreply.github.com>
2021-07-01 14:20:06 -0300

    Update ExplorarComIngre.js

--- src/pages/ExplorarComIngre.js
+++ src/pages/ExplorarComIngre.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../Components/Header';
-import MenuInferior  '../components/MenuInferior';
+import MenuInferior '../components/MenuInferior';
 
 function ExplorarComIngre() {
   return (
