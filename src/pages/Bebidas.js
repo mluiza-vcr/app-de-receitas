c8f10689363ab4b4928e56442c51ea1c76ec1d79
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-05 01:49:06 -0300

    Reqs 27,28,29,30

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DrinkList from '../components/DrinkList';
+import ButtonDrinkCategories from '../components/ButtonDrinkCategories';
 
 function Bebidas() {
   return (
     <div>
       <h2>Bebidas</h2>
+      <ButtonDrinkCategories />
       <DrinkList />
     </div>
   );
--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MealList from '../components/MealList';
+import ButtonMealCategories from '../components/ButtonMealCategories';
 
 function Comidas() {
   return (
     <div>
       <h2>Comidas</h2>
+      <ButtonMealCategories />
       <MealList />
     </div>
   );
