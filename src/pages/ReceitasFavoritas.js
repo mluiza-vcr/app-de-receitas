2f49dd364b1fe45c9c773ed8aaaf11addfe7a40b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-12 14:10:27 -0300

    Commit para poder fazer pull

--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReceitasCriadas from '../components/ReceitasCriadas';
 import Header from '../Components/Header';
 
 function ReceitasFavoritas() {
   return (
     <div>
       <Header title="Receitas Favoritas" />
+      <ReceitasCriadas />
     </div>
   );
 }
